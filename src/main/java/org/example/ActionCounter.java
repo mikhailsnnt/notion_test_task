0961ac3488a38673987e4f9925fb21a1f8c28e8d
Никитин Михаил <nikitinmd@elfin.tech>
2023-09-21 20:23:35 +0300

    Implement ActionCounter methods

--- src/main/java/org/example/ActionCounter.java
+++ src/main/java/org/example/ActionCounter.java
@@ -1,11 +1,68 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class ActionCounter {
-    public void call(int timestamp){
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final List<Integer> calls = new ArrayList<>();
+
+    public void call(int timestamp) {
+        if (timestamp < 0)
+            throw new IllegalArgumentException("Timestamp < 0");
+        Lock writeLock = readWriteLock.writeLock();
+        try {
+            writeLock.lock();
+            calls.add(timestamp);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
 
-    public int getActions(int timestamp){
-        return -1;
+    public int getActions(int timestamp) {
+        if (timestamp < 0)
+            throw new IllegalArgumentException("Timestamp < 0");
+        if (calls.isEmpty())
+            return 0;
+        Lock readLock = readWriteLock.readLock();
+        try {
+            readLock.lock();
+
+            int lTimestamp = binarySearchCallLeft(Math.max(timestamp - 300, 0));
+
+            int rTimestamp = binarySearchCallRight(timestamp);
+
+            return rTimestamp - lTimestamp;
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    private int binarySearchCallLeft(int t) {
+        int l = 0, r = calls.size(), mid;
+        while (l < r) {
+            mid = (l + r) >>> 1;
+            if (calls.get(mid) < t)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+
+    private int binarySearchCallRight(int t) {
+        int l = 0, r = calls.size(), mid;
+        while (l < r) {
+            mid = (l + r) >>> 1;
+            if (calls.get(mid) > t)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
     }
 }
--- src/test/java/org/example/ActionCounterTest.java
+++ src/test/java/org/example/ActionCounterTest.java
@@ -1,65 +1,26 @@
 package org.example;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ActionCounterTest {
 
-    @Test
-    void testOneThreadUniques(){
-        ActionCounter counter = new ActionCounter();
-        callCounter(counter,11,22,33,34,329);
-        assertEquals(3, counter.getActions(330));
+    @RepeatedTest(value = 5)
+    void testOnRandomVals(){
+        Random random = new Random();
+        CounterStupidProxy counter = new CounterStupidProxy();
+        callCounterRandom(counter, 100,1000);
+        int timestampToQuery = random.nextInt(300,1300);
+        int expected = counter.getStupidSolution(timestampToQuery);
+        assertEquals(expected, counter.getActions(timestampToQuery));
     }
 
-    @Test
-    void testOneThreadWithEquals(){
-        ActionCounter counter = new ActionCounter();
-        callCounter(counter,11,11,33,33,33,34,35,35,330,330);
-        assertEquals(8, counter.getActions(330));
-    }
-
-
-    @Test
-    void testMultiThreadedWithUniques() throws InterruptedException {
-        ActionCounter counter = new ActionCounter();
-        List<Thread> threads = List.of(
-                new Thread(() -> callCounter(counter, 100, 101, 102, 103, 104)),
-                new Thread(() -> callCounter(counter, 106, 107, 108, 109, 110)),
-                new Thread(() -> callCounter(counter, 111, 112, 113, 114, 115))
-        );
-        threads.forEach(Thread::start);
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        assertEquals(15, counter.getActions(400));
-    }
-
-
-    @Test
-    void testMultiThreadedWithEquals() throws InterruptedException {
-        ActionCounter counter = new ActionCounter();
-
-        List<Thread> threads = List.of(
-                new Thread(() -> callCounter(counter, 100, 100, 100, 100, 101)),
-                new Thread(() -> callCounter(counter, 101, 100, 101, 100, 101)),
-                new Thread(() -> callCounter(counter, 100, 101, 101, 100, 101))
-        );
-
-        threads.forEach(Thread::start);
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        assertEquals(15, counter.getActions(400));
-    }
 
     @Test
     void testLessCloseToMinSeconds(){
@@ -75,18 +36,29 @@ class ActionCounterTest {
     }
 
     @Test
+    void testOneCallMade(){
+        CounterStupidProxy counter = new CounterStupidProxy();
+        counter.call(500);
+        assertEquals(1, counter.getActions(500));
+    }
+
+    @Test
     void shouldThrowBadInput(){
         ActionCounter counter = new ActionCounter();
         assertThrows(IllegalArgumentException.class, ()-> counter.call(-1));
         assertThrows(IllegalArgumentException.class, ()-> counter.getActions(-9));
     }
 
-
-
+    private void callCounterRandom(ActionCounter counter, int minCalls, int maxCalls){
+        Random random = new Random();
+        Stream.generate(() -> random.nextInt(0,1000))
+                .limit(random.nextInt(minCalls , maxCalls))
+                .sorted()
+                .forEach(counter::call);
+    }
 
     private void callCounter(ActionCounter counter, Integer ... timestamps){
         for (Integer timestamp : timestamps)
             counter.call(timestamp);
     }
-
 }
